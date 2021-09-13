3f397a6116c35c4d789e2c9960c5a130bd7e4382
Evan Loiterman <evanloiterman@gmail.com>
2021-09-12 22:19:32 -0500

    more php exercises

--- php/hello.php
+++ php/hello.php
@@ -1,21 +1,13 @@
 <?php
-  $name = 'Bob Smith';
-  $greeting = "Hello {$name}";
-  $count=5;
-  $price=.6;
-  $total=($count*$price);
-  $msg = "I see you have {$count} oranges, that will be \${$total}";
+/**
+ * Greets the user with the current date and time.
+ */
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <title>Contact</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body>
-       <h1><?php echo $greeting; ?></h1>
-       <p><?php echo $msg;?></p>
-    </body>
-</html>
+//Create a date object http://php.net/manual/en/book.datetime.php from PHP's built
+$date = new DateTime();
+
+#Format the date http://php.net/manual/en/datetime.formats.date.php
+$formattedDate = $date->format('Y-m-d h:i:s');
+
+echo "Hello World it is {$formattedDate}";
+
